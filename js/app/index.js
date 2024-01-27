80328a2ac4a399a0f61f8be600a8dc07399e51d7
kiritter <kirittergithub@gmail.com>
2024-01-27 17:21:59 +0900

    refactor: Lazy loading pref_border

--- js/app/index.js
+++ js/app/index.js
@@ -18,7 +18,7 @@ async function initMyApp(global) {
     var routelineManager = new MyApp.RoutelineManager(gaChannel, mapBoth, MyApp.globalState, myChannel);
     routelineManager.init();
 
-    var prefBorderManager = new MyApp.PrefectureBorderManager(mapBoth, MyApp.globalState, myChannel);
+    var prefBorderManager = new MyApp.PrefectureBorderManager(gaChannel, mapBoth, MyApp.globalState, myChannel);
     prefBorderManager.init();
 
     var markerRecommend100MeizanManager = new MyApp.MarkerRecommend100MeizanManager(gaChannel, mapBoth);
